6c471dacdd3764a37c0463b5bc65119a0d594a6d
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-03-08 09:08:59 +0100

    Create new Book object through user input
    New Book will be added to an array.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, numPages, read) {
   this.title = title;
   this.author = author;
@@ -9,6 +11,19 @@ Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.read}`;
 }
 
+
+function addBookToLibrary() {
+  const title = prompt("Book title: ");
+  const author = prompt("Book author: ");
+  const numPages = prompt("Number of pages: ");
+  const readStatus = prompt("Have you read it? ");
+
+  const newBook = new Book(title, author, numPages, readStatus);
+  myLibrary.push(newBook);
+  test();
+}
+
+// Test data
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 196, "not read yet");
 
 console.log(theHobbit.info())
